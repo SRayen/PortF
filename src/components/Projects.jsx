56d2da53a80a543a77e56cbc852137c9b6eda17f
Selmen Rayen <selmanerayene95@gmail.com>
2023-03-17 19:52:59 +0100

    first

---
+++ src/components/Projects.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import { Row, Container, Col, Nav, Tab } from "react-bootstrap";
+import ProjectCard from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import TrackVisibility from "react-on-screen";
+import "animate.css";
+
+const Projects = () => {
+  const projects1 = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+  const projects2 = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+  const projects3 = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+          <TrackVisibility>
+            {({isVisible}) => 
+            <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+            <h2>Projects</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+              corrupti dolorem aspernatur fugiat laudantium, labore quibusdam
+              ut. Maiores officia libero qui minus! Accusantium in eaque quidem
+              quo, esse voluptatem quibusdam.
+            </p>
+            </div>}
+            </TrackVisibility>
+            {/* PILL */}
+            <Tab.Container id="projects-tab" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content align-items-center "
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects1.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="second">
+                  <Row>
+                    {projects2.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="third">
+                  <Row>
+                    {projects3.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="colorSharp2"
+      />
+    </section>
+  );
+};
+
+export default Projects;
